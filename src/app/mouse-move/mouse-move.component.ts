e5812fb7c531e9fd69da7b65741cc2a6fe9668c3
cxxyao2 <cxxyao2@gmail.com>
2021-12-31 21:19:03 -0500

    resize screen by mouse moving

--- src/app/mouse-move/mouse-move.component.ts
+++ src/app/mouse-move/mouse-move.component.ts
@@ -1,20 +1,46 @@
-import { Component, OnInit,HostListener } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostListener,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-mouse-move',
   templateUrl: './mouse-move.component.html',
-  styleUrls: ['./mouse-move.component.css']
+  styleUrls: ['./mouse-move.component.css'],
 })
 export class MouseMoveComponent implements OnInit {
+  @ViewChild('leftChild') leftDiv!: ElementRef;
+  @ViewChild('rightChild') rightDiv!: ElementRef;
 
-  constructor() { }
+  canResizing = false;
+  initX = 0;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    if (!this.canResizing) return;
+    let diff = e.clientX - this.initX;
+    this.initX = e.clientX;
+    let leftChildWidth = this.leftDiv.nativeElement.offsetWidth + diff;
+    let rightChildWidth = this.rightDiv.nativeElement.offsetWidth - diff;
+    this.leftDiv.nativeElement.style.width = leftChildWidth + 'px';
+    this.rightDiv.nativeElement.style.width = rightChildWidth + 'px';
   }
 
-  @HostListener('document:mousemove',['$event'])
-  onMouseMove(e:any){
-    console.log(e);
+  @HostListener('document:mouseup', ['$event'])
+  onMouseUp(e: MouseEvent) {
+    // console.log(e);
+    this.canResizing = false;
   }
 
+  onMouseDown(e: MouseEvent) {
+    this.canResizing = true;
+    this.initX = e.clientX;
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-     userName: 'undefined',
-   production: 'undefined'
+  production: false,
 };
